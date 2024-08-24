c1ad5a4f7073d810ffd99164f5318f6d1992760b
Varun Tomar <vrnkmr119@gmail.com>
2024-08-24 23:55:15 +0530

    added search service

---
+++ AuctionWheels.API/Search.Service/Controllers/SearchController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Entities;
+using Search.Service.Helpers;
+using Search.Service.Models;
+
+namespace Search.Service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SearchController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Item>>> SearchItems([FromQuery] SearchParam searchParam)
+        {
+            var query = DB.PagedSearch<Item, Item>();
+
+            query.Sort(x => x.Ascending(a => a.Make));
+
+            if (!string.IsNullOrWhiteSpace(searchParam.SearchTerm))
+            {
+                query.Match(MongoDB.Entities.Search.Full, searchParam.SearchTerm).SortByTextScore();
+            }
+
+            query = searchParam.OrderBy switch
+            {
+                "make" => query.Sort(x => x.Ascending(a => a.Make)),
+                "new" => query.Sort(x => x.Ascending(a => a.CreatedAt)),
+                _ => query.Sort(x => x.Ascending(a => a.AuctionEnd))
+            };
+
+            query = searchParam.FilterBy switch
+            {
+                "finished" => query.Match(x => x.AuctionEnd < DateTime.UtcNow),
+                "endingSoon" => query.Match(x => x.AuctionEnd < DateTime.UtcNow.AddHours(6) && x.AuctionEnd > DateTime.UtcNow),
+                _ => query.Match(x => x.AuctionEnd > DateTime.UtcNow)
+            };
+
+            if (!string.IsNullOrWhiteSpace(searchParam.Seller))
+            {
+                query.Match(x => x.Seller == searchParam.Seller);
+            }
+
+            if (!string.IsNullOrWhiteSpace(searchParam.Winner))
+            {
+                query.Match(x => x.Winner == searchParam.Winner);
+            }
+
+            query.PageNumber(searchParam.PageNumber);
+            query.PageSize(searchParam.PageSize);
+
+            var result = await query.ExecuteAsync();
+
+            return Ok(new { results = result.Results, pageCount = result.PageCount, totalCount = result.TotalCount });
+        }
+    }
+}
---
+++ AuctionWheels.API/Search.Service/Data/DbInitializer.cs
@@ -0,0 +1,38 @@
+﻿using MongoDB.Driver;
+using MongoDB.Entities;
+using Search.Service.Models;
+using System.Text.Json;
+
+namespace Search.Service.Data
+{
+    public class DbInitializer
+    {
+        public static async Task InitDb(WebApplication app)
+        {
+            await DB.InitAsync("SearchDb", MongoClientSettings.FromConnectionString(app.Configuration.GetConnectionString("MongoDbConnectionString")));
+
+            await DB.Index<Item>()
+                .Key(x => x.Make, KeyType.Text)
+                .Key(x => x.Model, KeyType.Text)
+                .Key(x => x.Color, KeyType.Text)
+                .CreateAsync();
+
+            var count = await DB.CountAsync<Item>();
+
+            if (count == 0)
+            {
+                Console.WriteLine("Data already present!");
+                var itemData = await File.ReadAllTextAsync("Data/Auction.json");
+
+                JsonSerializerOptions options = new()
+                {
+                    PropertyNameCaseInsensitive = true,
+                };
+
+                var items = JsonSerializer.Deserialize<List<Item>>(itemData, options);
+
+                await DB.SaveAsync(items!);
+            }
+        }
+    }
+}
