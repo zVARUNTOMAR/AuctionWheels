34e46d38ddd4594f6b6c05e821b76f86db7efa48
Varun Tomar <vrnkmr119@gmail.com>
2024-12-28 15:24:53 +0530

    Refactor auction listings to use hooks for data fetching and pagination; create separate auctionActions for API calls

--- AutionWheels.UI/auctionwheelsui/app/auctions/Listings.tsx
+++ AutionWheels.UI/auctionwheelsui/app/auctions/Listings.tsx
@@ -1,33 +1,38 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import AuctionCard from "./AuctionCard";
-import { Auction, PagedResult } from "../types";
 import AppPagination from "../components/AppPagination";
+import { Auction } from "../types";
+import { getData } from "../actions/auctionActions";
 
-async function getData(): Promise<PagedResult<Auction>> {
-  const res = await fetch("http://localhost:6001/search?pageSize=8");
+export default function Listings() {
+  const [auctions, setAuctions] = useState<Auction[]>([]);
+  const [pageCount, setPageCount] = useState(0);
+  const [pageNumber, setPageNumber] = useState(1);
 
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
+  useEffect(() => {
+    getData(pageNumber).then((data) => {
+      setAuctions(data.results);
+      setPageCount(data.pageCount);
+    });
+  }, [pageNumber]);
 
-  return res.json();
-}
-
-export default async function Listings() {
-  const data = await getData();
+  if (auctions.length === 0) return <h2>Loading...</h2>;
 
   return (
     <>
       <div className="grid grid-cols-1 gap-7 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        {data &&
-          data.results.map((auction) => (
+        {auctions &&
+          auctions.map((auction) => (
             <AuctionCard auction={auction} key={auction.id}></AuctionCard>
           ))}
       </div>
       <div className="flex justify-center mt-4">
         <AppPagination
-          currentPage={1}
-          pageCount={data.pageCount}
+          currentPage={pageNumber}
+          pageCount={pageCount}
+          pageChanged={setPageNumber}
         ></AppPagination>
       </div>
     </>
--- AutionWheels.UI/auctionwheelsui/app/components/AppPagination.tsx
+++ AutionWheels.UI/auctionwheelsui/app/components/AppPagination.tsx
@@ -6,19 +6,18 @@ import React from "react";
 type Props = {
   currentPage: number;
   pageCount: number;
+  pageChanged: (page: number) => void;
 };
 
-const AppPagination = ({ currentPage, pageCount }: Props) => {
-  const [pageNumber, setPageNumber] = React.useState(currentPage);
-
+const AppPagination = ({ currentPage, pageCount, pageChanged }: Props) => {
   return (
     <Pagination
-      currentPage={pageNumber}
-      onPageChange={(e) => setPageNumber(e)}
+      currentPage={currentPage}
+      onPageChange={(e) => pageChanged(e)}
       totalPages={pageCount}
       layout="pagination"
       showIcons={true}
-      //   className="text-blue-500 mb-5"
+      className="text-blue-500 mb-5"
     ></Pagination>
   );
 };
